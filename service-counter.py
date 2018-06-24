5ce3fbf67671c50c6f3196ec7d103fb479b22b0d
meirfi <meirfi@outlook.com>
2018-06-25 00:12:23 +0300

    Changing Script Functionality

--- service-counter.py
+++ service-counter.py
@@ -1,30 +1,89 @@
-import sys
+#!/usr/bin/env python
 
-def apache_output(line):
-    split_line = line.split()
-    return {'remote_host': split_line[0],
-            'apache_status': split_line[8],
-            'data_transfer': split_line[9],
-    }
+import os.path
+
+class NginxLogAnalyzer():
+
+    def __init__(self, readfile, writefile, topcount=5):
+
+        self.summary = {
+            "requests": {},
+            "ips": {},
+            "useragents": {}
+        }
+
+        self.topcount = topcount
+
+        self.reafile = readfile
+        self.writefile = writefile
+
+    def log_analyze(self):
+
+        if not os.path.isfile(self.reafile):
+            print(self.reafile, "does not exist! exiting")
+            exit(1)
+
+        log = open(self.reafile, 'r')
+        lines = log.readlines()
+        log.close()
+        loglist = []
+
+        for s in lines:
+            line = s.strip()
+            tmp = line.split(' ')
+            ip = tmp[0]
+
+            doublequotes = NginxLogAnalyzer.find_chars(line, '"')
+            request_start = doublequotes[0]+1
+            request_end = doublequotes[1]
+            useragent_start = doublequotes[4]+1
+            useragent_end = doublequotes[5]
+
+            request = line[request_start:request_end]
+            useragent = line[useragent_start:useragent_end]
+
+            loglist.append({
+                "ip": ip,
+                "request": request,
+                "useragent": useragent
+            })
+
+        self.summarize(loglist)
+        self.write_summary()
+
+    def summarize(self, cols):
+        for col in cols:
+            if not col['request'] in self.summary['requests']:
+                self.summary['requests'][col['request']] = 0
+            self.summary['requests'][col['request']] += 1
+
+            if not col['ip'] in self.summary['ips']:
+                self.summary['ips'][col['ip']] = 0
+            self.summary['ips'][col['ip']] += 1
+
+            if not col['useragent'] in self.summary['useragents']:
+                self.summary['useragents'][col['useragent']] = 0
+            self.summary['useragents'][col['useragent']] += 1
+
+    def write_summary(self):
+        summary = open(self.writefile, 'w')
+        summary.write("Log summary\n")
+        for key in self.summary:
+            list = sorted(self.summary[key].items(), key=lambda x: x[1], reverse=True)
+            list = list[:self.topcount]
+            summary.write("\nTop "+key+":\n")
+            for l in list:
+                summary.write(l[0]+": "+str(l[1])+" times\n")
+        summary.close()
+
+    @staticmethod
+    def find_chars(string, char):
+
+        return [i for i, ltr in enumerate(string) if ltr == char]
 
 
-def final_report(logfile):
-    for line in logfile:
-        line_dict = apache_output(line)
-        print(line_dict)
-
-
-if __name__ == "__main__":
-    if not len(sys.argv) > 1:
-        print (__doc__)
-        sys.exit(1)
-    infile_name = sys.argv[1]
-    try:
-        infile = open(infile_name, 'r')
-    except IOError:
-        print ("You must specify a valid file to parse")
-        print (__doc__)
-        sys.exit(1)
-    log_report = final_report(infile)
-    print (log_report)
-    infile.close()
+if __name__ == '__main__':
+    logfile = '/var/log/nginx/access.log'
+    summaryfile = './access_summary.log'
+    summary = NginxLogAnalyzer(logfile, summaryfile, 5)
+    summary.log_analyze()
